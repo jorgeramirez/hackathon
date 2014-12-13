f85f2da7c90b92e48021eedfaeedbc20a129fe44
Jorge Ramírez <jorgeramirez1990@gmail.com>
2014-12-13 18:43:35 -0300

    Bugfix en controller de convocatorias

--- app/assets/javascripts/controllers/convocatorias_controller.js
+++ app/assets/javascripts/controllers/convocatorias_controller.js
@@ -11,45 +11,6 @@ Bursa.ConvocatoriasBaseController = Ember.ObjectController.extend({
 });
 
 
-
-Bursa.ConvocatoriasNewController = Bursa.ConvocatoriasBaseController.extend({
-
-  formTitle: 'Nuevo Usuario',
-
-  actions: {
-    save: function() {
-      var model = this.get('model');
-      var self = this;
-			Bursa.Utils.disableElement('button[type=submit]');
-        
-			if (model.get('isValid')) {
-				model.save().then(function(response) {
-					// success
-					self.set('model', response);
-					self.transitionToRoute('usuarios').then(function () {
-						Bursa.Notification.show('Exito', 'El usuario se ha guardado.');
-						Bursa.Utils.enableElement('button[type=submit]');
-					});
-				}, function(response){
-					Bursa.Notification.show('Error', 'No se pudo crear el usuario.', Bursa.Notification.ERROR_MSG);
-					Bursa.Utils.enableElement('button[type=submit]');
-				});
-			}else{
-				Bursa.Utils.enableElement('button[type=submit]');
-			}
-        
-      
-      
-    },
-
-    cancel: function() {
-      var model = this.get('model');
-      model.deleteRecord();
-      this.transitionToRoute('usuarios');
-    }
-  }
-});
-
 Bursa.ConvocatoriaConsultarController = Bursa.ConvocatoriasBaseController.extend({
 
   formTitle: 'Consultas',
@@ -86,26 +47,10 @@ Bursa.ConvocatoriaConsultarController = Bursa.ConvocatoriasBaseController.extend
 Bursa.ConvocatoriaVerController = Ember.ObjectController.extend({
 
   actions: {
-    deleteRecord: function() {
-      var model = this.get('model');
-      var self = this;
-			Bursa.Utils.disableElement('button[type=submit]');
-      model.deleteRecord();
-      model.save().then(function() {
-        self.transitionToRoute('usuarios').then(function () {
-          Bursa.Notification.show('Exito', 'El usuario se ha eliminado.');
-          Bursa.Utils.enableElement('button[type=submit]');
-        });
-      }, function(response) {
-
-				Bursa.Notification.show('Error', 'No se pudo borrar el usuario.', Bursa.Notification.ERROR_MSG);
-        Bursa.Utils.enableElement('button[type=submit]');
-      });
-    },
-
+    
     cancel: function() {
 			$('body').removeClass('modal-open');
-      this.transitionToRoute('usuarios');
+      this.transitionToRoute('convocatorias');
     }
   }
 });
