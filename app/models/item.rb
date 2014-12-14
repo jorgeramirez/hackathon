daa23cedd9fdb5101d5fb7a94e7870a8b5ad8db0
Gabriela Gaona <gaby.lorely@gmail.com>
2014-12-14 15:20:44 -0300

    Agregue convocantes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
 
-  scope :by_descripcion, -> descripcion { where("producto_descripcion ilike ?", "%#{descripcion}%") }
+  scope :by_item, -> descripcion { where("producto_descripcion ilike ? and esconvocante=false", "%#{descripcion}%") }
+  scope :by_convocante, -> descripcion { where("producto_descripcion ilike ? and esconvocante=true", "%#{descripcion}%") }
 end
